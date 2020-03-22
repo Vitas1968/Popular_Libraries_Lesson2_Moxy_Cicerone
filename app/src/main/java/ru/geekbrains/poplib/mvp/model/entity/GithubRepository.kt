a8721f7a05035d8f8dafcaa7d405f725cde924f3
Vitas1968 <dor1968@mail.ru>
2020-03-22 22:12:40 +0700

    пришлось делать его Parcelable для передачи через Bundle

--- app/src/main/java/ru/geekbrains/poplib/mvp/model/entity/GithubRepository.kt
+++ app/src/main/java/ru/geekbrains/poplib/mvp/model/entity/GithubRepository.kt
@@ -1,8 +1,38 @@
 package ru.geekbrains.poplib.mvp.model.entity
 
+import android.os.Parcel
+import android.os.Parcelable
+
 //https://api.github.com/users/googlesamples/repos
 data class GithubRepository(
     val id: String,
     val name: String,
     val forksCount: Int
-)
+): Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString().toString(),
+        parcel.readString().toString(),
+        parcel.readInt()
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(id)
+        parcel.writeString(name)
+        parcel.writeInt(forksCount)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<GithubRepository> {
+        override fun createFromParcel(parcel: Parcel): GithubRepository {
+            return GithubRepository(parcel)
+        }
+
+        override fun newArray(size: Int): Array<GithubRepository?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
