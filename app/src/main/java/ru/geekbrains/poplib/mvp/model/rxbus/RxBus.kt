cd6a90189cf321873215774e4b94ce807ebe498c
Vitas1968 <dor1968@mail.ru>
2020-03-24 12:37:29 +0700

    импорт

--- app/src/main/java/ru/geekbrains/poplib/mvp/model/rxbus/RxBus.kt
+++ app/src/main/java/ru/geekbrains/poplib/mvp/model/rxbus/RxBus.kt
@@ -1,20 +1,21 @@
 package ru.geekbrains.poplib.mvp.model.rxbus
 
 import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.schedulers.Schedulers
 import io.reactivex.rxjava3.subjects.PublishSubject
 import ru.geekbrains.poplib.mvp.model.entity.GithubRepository
 
 
 class RxBus {
-    private val bus = PublishSubject.create<List<GithubRepository>>()
+        private val bus = PublishSubject.create<List<GithubRepository>>()
 
-    fun send(list: List<GithubRepository>) {
-        bus.onNext(list)
-    }
+        fun send(list: List<GithubRepository>) {
+            bus.onNext(list)
+        }
 
-    fun toObservable(): Observable<List<GithubRepository>> {
-        return bus
-    }
+        fun toObservable(): Observable<List<GithubRepository>> {
+            return bus
+        }
 
 
 
