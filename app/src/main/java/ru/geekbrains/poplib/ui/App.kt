a11ea2a1cb17999004554176218b24be698395b2
Vitas1968 <dor1968@mail.ru>
2020-03-26 10:30:08 +0700

    создание экземпляра RxBus

--- app/src/main/java/ru/geekbrains/poplib/ui/App.kt
+++ app/src/main/java/ru/geekbrains/poplib/ui/App.kt
@@ -1,13 +1,17 @@
 package ru.geekbrains.poplib.ui
 
 import android.app.Application
+import ru.geekbrains.poplib.mvp.model.rxbus.RxBus
 import ru.terrakok.cicerone.Cicerone
 import ru.terrakok.cicerone.Router
+
 
 class App : Application() {
     companion object {
         lateinit var instance: App
     }
+
+    private var bus: RxBus? = null
 
     val cicerone: Cicerone<Router> by lazy {
         Cicerone.create()
@@ -16,9 +20,13 @@ class App : Application() {
     override fun onCreate() {
         super.onCreate()
         instance = this
+        bus = RxBus()
     }
 
 
+    fun bus(): RxBus? {
+        return bus
+    }
     fun getNavigatorHolder() = cicerone.navigatorHolder
     fun getRouter() = cicerone.router
 }
