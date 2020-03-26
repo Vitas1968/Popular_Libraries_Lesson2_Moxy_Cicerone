053b4e147da7028c7086e8779224eabf242c3654
Vitas1968 <dor1968@mail.ru>
2020-03-26 10:29:20 +0700

    реализация RxBus

--- app/src/main/java/ru/geekbrains/poplib/mvp/model/rxbus/RxBus.kt
+++ app/src/main/java/ru/geekbrains/poplib/mvp/model/rxbus/RxBus.kt
@@ -1,4 +1,19 @@
 package ru.geekbrains.poplib.mvp.model.rxbus
 
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.subjects.PublishSubject
+
+
+
+
 class RxBus {
+    private val bus = PublishSubject.create<Any>()
+
+    fun send(o: Any) {
+        bus.onNext(o)
+    }
+
+    fun toObservable(): Observable<Any>? {
+        return bus
+    }
 }
